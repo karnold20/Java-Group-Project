2644f4d445a83069a2ec8a30286162339401ab63
rukia18 <73853713+rukia18@users.noreply.github.com>
2020-11-14 15:26:03 -0600

    Update ControllerB.java

--- HangMan_FinalProject/ControllerB.java
+++ HangMan_FinalProject/ControllerB.java
@@ -1,1 +1,23 @@
-
+package hangman;
+//imports....
+public class Controller {
+  @FXML
+  private Label wordState;
+  @FXML
+  private TextField myGuess;
+  
+  StringProperty wordStateProperty = new StringProperty();
+  StringProperty secretWordProperty = new StringProperty();
+  StringProperty myGuessProperty = new StringProperty();
+  
+  public void onButtonClicked(){
+    if(guessIsRight(myGuessProperty,secretWordProperty,wordStateProperty)){
+      wordState.bind(wordStateProperty); //update wordState
+    }
+    else{
+      //update hangmanImage
+    }
+  }
+  public bool guessIsRight(StringProperty myGuess,StringProperty secret,StringProperty word){
+  }
+}//end of Controller
