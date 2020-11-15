ce4de7d527abe828db9aad843ca0d8821243a1f1
Lorenzo McDaniel <39344145+lorenzoMcD@users.noreply.github.com>
2020-11-14 18:41:03 -0600

    Nov 14
    setup the button event for the guessFullword button.

--- HangMan_FinalProject/application/controlB.java
+++ HangMan_FinalProject/application/controlB.java
@@ -9,6 +9,8 @@ import javafx.scene.shape.Line;
 
 public class controlB {
 
+	public String mysteryWord;
+	public int numGuesses = 6;
 	
 	@FXML
 	private Label Category_label; // used to grab text from the 
@@ -71,6 +73,8 @@ public class controlB {
 
 		// grab random word 
 		
+		// set mysteryWord
+		mysteryWord = "apple";
 		
 	}
 	
@@ -90,6 +94,61 @@ public class controlB {
 		
 	}
 	
+	// checks the number of guess and makes hangman limbs
+	// visible depending on how many guesses remain
+	public void makeVisible(int guess) {
+		
+		// 
+		if (guess == 5)
+		{
+			
+			headShape.setVisible(true);
+		}
+		else if (guess == 4)
+		{
+			
+			spine.setVisible(true);
+		}
+		else if (guess == 3)
+		{
+			
+			leftArmLine.setVisible(true);
+		}
+		else if (guess == 2)
+		{
+			
+			rightArmLine.setVisible(true);
+		}
+		else if (guess == 1)
+		{
+			
+			leftLegLine.setVisible(true);
+		}
+		else if (guess == 0)
+		{
+			
+			rightLegLine.setVisible(true);
+		
+		
+			// game is over disable the textfield and two buttons
+			// and output that user lost game in the textfield
+			
+			guessBttn.setVisible(false);
+			
+			guessFullWord.setVisible(false);
+			
+			
+			AnswerBox.setDisable(true); // 
+			
+			AnswerBox.setText("You lost");	
+			
+			
+		}
+		
+		
+	}
+	
+	
 	
 	// used to grab the category word from the first scenes controller
 	public void SetCategory(String text) {
@@ -97,6 +156,62 @@ public class controlB {
 		
 		Category_label.setText(text);
 	}
+	
+	
+	
+	
+	// button event for guessing the entire word 
+	public void GuessFullWordEvent(String text) {
+		
+		
+		
+		
+	}
+	
+
+	
+	
+	/*
+	
+	public void onButtonClicked(){
+		if(guessIsRight(mysteryWord,AnswerBox.toString())){
+			wordDashes = updateWordState(mysteryWord,AnswerBox.toString(),wordDashes);
+		}
+		else{
+			//update hangman picture
+			
+			numGuesses--;
+			makeVisible(numGuesses);
+			
+		
+		}
+	}
+	
+	//checks if letter is in secret word
+	public boolean guessIsRight(String secretWord,String guess){
+		for (int i=0; i<secretWord.length(); i++){
+			String secretLetter = secretWord.charAt(i);
+			if (secretLetter.equals(guess)){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	//updates word state
+	public void updateWordState(String secretWord,String word,String guess){
+		for (int i=0; i<wordDashes.toString().length(); i++){
+			String secretLetter = secretWord.charAt(i);
+			if (secretLetter.equals(guess)){
+				//set letter at X index
+			}
+		}	
+	}
+	
+	
+	*/
+	
+	
 	
 	
 
